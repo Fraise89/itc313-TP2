a3c39f50f949b5ec20012f15e19f2ccab8844e35
zephyr-bat <northwind@outlook.fr>
2019-12-17 15:17:52 +0100

    correction Question 2 OK

--- magasin.cpp
+++ magasin.cpp
@@ -1,8 +1,32 @@
-#include <string>
 #include "magasin.h"
-Magasin::Magasin(){
-	
+
+Magasin::Magasin(vector<Product*> products, vector<Client*> clients, vector<Order*> m_orders){
+	m_products = products;
+	m_clients = clients;
+	m_orders = orders
 }
-Object Magasin::getObject(std::string Object) {
-	return Object;
+
+vector<Product*> Magasin::getProducts(){
+	return m_products;
 }
+vector<Client*> Magasin::getClients(){
+        return m_clients;
+}
+
+vector<Order*> Magasin::getOrder(){
+        return m_orders;
+}
+
+void Magasin::setProducts(vector<Product*> products){
+	m_product = products;
+}
+void Magasin::setClients(vector<Client*> clients){
+        m_clients = clients;
+}
+void Magasin::setOrders(vector<Order*> orders){
+        m_orders = orders;
+}
+
+
+
+
